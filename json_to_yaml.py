1b4217f023c455dc3ba75bdf94bc9271130a1baa
Ran Hershko <ran.hershko@gmail.com>
2019-12-15 11:47:14 +0200

    # Use Constants to improve code readability # Check file existence's and print error output if not # people_list items are person name and age instead of only name # A person_name instead of i => more meaning for a variable name # Change from nested if # The next run will be from current person - no need to run on the one that printed already # The primary function should be smaller and that way it leave an option to import this module # variables renaming

--- json_to_yaml.py
+++ json_to_yaml.py
@@ -19,17 +19,17 @@ def yaml_my_json_group_sorted():
         print("Unexpected error:", sys.exc_info()[0])
         exit(100)
 
-    ages = data['buckets']
+    ages_borders = data['buckets']
     people_list = sorted(data["ppl_ages"].items(), key=lambda kv: kv[AGE])
     extra_age = [0, people_list[LAST_INDEX][AGE] + 1]
-    ages.extend(extra_age)
-    sorted_ages = sorted(ages)
+    ages_borders.extend(extra_age)
+    sorted_ages_broders = sorted(ages_borders)
 
     current_people_list = people_list
-    for min_age_border in range(len(sorted_ages) - 1):
-        print(sorted_ages[min_age_border], "-", sorted_ages[min_age_border + NEXT_AGE_BORDER], ":", sep="")
+    for age_border in range(len(sorted_ages_broders) - 1):
+        print(sorted_ages_broders[age_border], "-", sorted_ages_broders[age_border + NEXT_AGE_BORDER], ":", sep="")
         for person in current_people_list:
-            if sorted_ages[min_age_border] <= person[AGE] < sorted_ages[min_age_border + NEXT_AGE_BORDER]:
+            if sorted_ages_broders[age_border] <= person[AGE] < sorted_ages_broders[age_border + NEXT_AGE_BORDER]:
                 print("  -", person[NAME])
             else:
                 current_people_list = people_list[people_list.index(person):]
